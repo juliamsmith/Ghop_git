4bcbf44d3736f0bd1dc2fbff7aa973f13d85ac3f
juliamsmith <40371272+juliamsmith@users.noreply.github.com>
2023-11-01 09:56:44 -0700

    Start coding to see components (not finished)

--- biophys/biophys_analysis.R
+++ biophys/biophys_analysis.R
@@ -2,7 +2,7 @@ library(readr)
 library(tidyverse)
 library(TrenchR)
 
-wsdzoom <- read_csv("zoomwsmidAug.csv")
+wsdzoom <- read_csv("biophys/zoomwsmidAug.csv")
 
 wsdzoom_s <- wsdzoom %>% spread(clim_var, value)
 
@@ -63,4 +63,79 @@ wsdzoomtb <- wsdtb_s %>% gather("clim_var", "value", -dt, -site)
 
 #comparing soil temp and predicted Tb
 ggplot(wsdzoomtb %>% filter((clim_var=="Tb_pred" | clim_var=="T_soil") & value<100), aes(x=dt, y=value, color=site, lty=clim_var)) + geom_line()
+
+#ZOOMING IN ON NAs by adding components of energy budget -- not resolved... still need to do more 
+
+Ts <- wsdtb_s[382,]
+
+T_a=air_temp_profile_neutral(T_r = Ts$T_0.25,
+                              zr  = 0.25,
+                              z0  = surf,
+                              z   = .01, # .001,
+                              T_s = Ts$T_soil)
+T_g, 
+u, 
+S, 
+K_t, 
+psi, 
+l, 
+Acondfact = 0.25, 
+z = 0.001, 
+          
+abs = 0.7, 
+r_g = 0.3 
+
+  stopifnot(u >= 0, S >= 0, K_t >= 0, K_t <= 1, psi >= -90, 
+            psi <= 90, l >= 0, Acondfact >= 0, Acondfact <= 1, z >= 
+              0, abs >= 0, abs <= 1, r_g >= 0, r_g <= 1)
+  T_a <- celsius_to_kelvin(T_a)
+  T_g <- celsius_to_kelvin(T_g)
+  sigma <- stefan_boltzmann_constant()
+  epsilon <- 1
+  Kf <- 0.025
+  v <- 15.68 * 10^-6
+  c <- l/2
+  a <- (0.365 + 0.241 * l * 1000)/1000
+  e <- sqrt(1 - a^2/c^2)
+  A <- 2 * pi * a^2 + 2 * pi * a * c/e * asin(e)
+  kd <- 1 - 0.09 * K_t
+  kd[K_t > 0.22 & K_t <= 0.8] <- 0.9511 - 0.1604 * K_t + 4.388 * 
+    K_t^2 - 16.638 * K_t^3 + 12.336 * K_t^4
+  kd[K_t > 0.8] <- 0.165
+  Sttl <- S
+  Sdir <- Sttl * (1 - kd)
+  Sdif <- Sttl * kd
+  psi_r <- degrees_to_radians(psi)
+  Re <- u * l/v
+  Nu <- 0.41 * Re^0.5
+  h_c <- Nu * Kf/l
+  hc_s <- h_c * (-0.007 * z/l + 1.71)
+  Thick <- 6 * 10^(-5)
+  hcut <- 0.15
+  Acond <- A * Acondfact
+  sa <- 0.19 - 0.00173 * psi
+  Adir <- A * sa
+  Aref <- Adir
+  Qdir <- abs * Adir * Sdir/cos(psi_r)
+  Qdif <- abs * Aref * Sdif
+  Qref <- r_g * Aref * Sttl
+  Qabs <- Qdir + Qdif + Qref
+  T_sky <- 0.0552 * (T_a)^1.5
+  a <- A * epsilon * sigma
+  b <- hc_s * A + hcut * Acond/Thick
+  d <- hc_s * A * T_a + 0.5 * A * epsilon * sigma * (T_sky^4 + 
+                                                       T_g^4) + hcut * Acond * T_g/Thick + Qabs
+  T_b <- 1/2 * sqrt((2 * b)/(a * sqrt((sqrt(3) * sqrt(256 * 
+                                                        a^3 * d^3 + 27 * a^2 * b^4) + 9 * a * b^2)^(1/3)/(2^(1/3) * 
+                                                                                                            3^(2/3) * a) - (4 * (2/3)^(1/3) * d)/(sqrt(3) * sqrt(256 * 
+                                                                                                                                                                   a^3 * d^3 + 27 * a^2 * b^4) + 9 * a * b^2)^(1/3))) - 
+                      (sqrt(3) * sqrt(256 * a^3 * d^3 + 27 * a^2 * b^4) + 
+                         9 * a * b^2)^(1/3)/(2^(1/3) * 3^(2/3) * a) + (4 * 
+                                                                         (2/3)^(1/3) * d)/(sqrt(3) * sqrt(256 * a^3 * d^3 + 27 * 
+                                                                                                            a^2 * b^4) + 9 * a * b^2)^(1/3)) - 1/2 * sqrt((sqrt(3) * 
+                                                                                                                                                             sqrt(256 * a^3 * d^3 + 27 * a^2 * b^4) + 9 * a * b^2)^(1/3)/(2^(1/3) * 
+                                                                                                                                                                                                                            3^(2/3) * a) - (4 * (2/3)^(1/3) * d)/(sqrt(3) * sqrt(256 * 
+                                                                                                                                                                                                                                                                                   a^3 * d^3 + 27 * a^2 * b^4) + 9 * a * b^2)^(1/3))
+  T_b[which(is.na(T_b))] <- NA
+  kelvin_to_celsius(T_b)
 
