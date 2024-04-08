07e05abc79570cc75cace912c8f9ec8b2f4f6f1f
juliamsmith <40371272+juliamsmith@users.noreply.github.com>
2024-04-08 14:28:14 -0700

    A patch to get air temps at low heights

---
+++ biophys/climuse_patch.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(TrenchR)
+
+
+#library(rstudioapi)
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
+## ^use this or set wd to source file location (which should be in biophys)
+
+climateuse <- readRDS("climateuse.rds")
+
+#Getting surface roughness estimate from C1 2022
+C1wsprofile <- read_csv("G:/Shared drives/RoL_FitnessConstraints/data/Transplant2022/C1_2022_surfroughness_mod.csv")
+wslow <- mean(C1wsprofile$windspeed1)
+wsmed <- mean(C1wsprofile$windspeed2)
+wshi <- mean(C1wsprofile$windspeed3)
+surf_r <- surface_roughness(u_r=c(wslow,	wsmed,	wshi), zr=c(.57, .82, 1.05))
+
+#set z, right now it's 5cm off the ground
+climateuse <- climateuse %>% mutate(T_lowuse=air_temp_profile(T_r=T_1.00use, u_r=.5, zr=2.5, z0=surf_r, z=.05, T_s=T_soil)) #FOR NOW putting air temp for soil temp because it's unlikely to affect it much at 1m
+
+#removing this one pesky entry... only run this once!
+climateuse <- climateuse[-1,]
+
+
+climateuse$wsuse <- .5 #wsuse is windspeed at 1m
+
+#now climateuse is free to use!
